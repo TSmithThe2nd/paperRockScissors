3402960e3fb81f463a67eeae8cbd95a10cd93442
Thomas W. Smith <82388542+TSmithThe2nd@users.noreply.github.com>
2022-02-14 17:17:08 -0500

    Removes unused code and commits

--- rock.papper.sciccors.py
+++ rock.papper.sciccors.py
@@ -1,7 +1,4 @@
-#failed code below to look back on your mistakes
 #paper rock scicors simulator for those too busy to play a game of paper rock sciccors.
-
-
 #brings in random number module
 import random
 
@@ -30,9 +27,7 @@ def game():
  
         if p1==p2:
             numberOfGames+=1
-            
-            
-
+                      
         elif  p1=='rock' and p2=='sciccors' or p1=='paper' and p2=='rock' or p1=='sciccors' and p2=='rock':
             p1=hands[random.randrange(3)]
             p2=hands[random.randrange(3)]
@@ -66,63 +61,6 @@ def game():
         game()    
 
 
-
 game()
 
        
-
-
-    
-
-    
-#functions to run game
-#function not yet successful. Functuion should run random number,
-#selecting paper rock or sccicors from hands
-# add to numberOfgames, and add to games if there is no tie.
-#I have an outline of how this logic should flow, but have not been able to
-#excute the loop as desired. it seems to select on if statement then continue
-#instead of restarting the loop
-
-
-
-
-
-#failed code
-
-#import random
-
-#def game():
-#varible for program
-##hands=['rock', 'paper', 'sciccors']
-##p1=hands[random.randrange(3)]
-##p2=hands[random.randrange(3)]
-##
-
-##games=0
-##numberOfGames=0
-##
-##p1w=0
-##p2w=0
-
-##    while games<5:
-##        if p1==p2:
-##            games+=1
-##            print(p1w)
-##            game()
-##            
-##        if p1=='rock' and p2=='sciccors' or p1=='paper' and p2=='rock' or p1=='sciccors' and p2=='rock':
-##            p1w+=1
-##            games=+1
-##            print(p1w)
-##            game()
-##        elif p2=='rock' and p1=='sciccors' or p2=='paper' and p1=='rock' or p2=='sciccors' and p1=='rock':
-##            p2w+=1
-##            games=+1
-##        else:
-##            games+=1
-##            print(p1w)
-##            game()
-##game()
-##    
-##
-##print(p1w)
